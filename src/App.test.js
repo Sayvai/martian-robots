a69b393acf5a7c736fe102915c565f211014b90c
Sayvai <sayvai@hotmail.com>
2023-02-14 21:01:44 +0000

    chore: add unit test for lost robot scenario
    included a new test to cater for LOST message, and reformatted existing unit tests.

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,9 @@ import { fireEvent, prettyDOM, render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("Martian Robots", () => {
-  it("renders textare and submit button", () => {
+  it(`hould
+    - render textarea,
+    - render submit button`, () => {
     // given
     render(<App />);
 
@@ -16,7 +18,10 @@ describe("Martian Robots", () => {
     expect(buttonElement).toBeInTheDocument();
   });
 
-  it("should enter instructions, submit, and return result for one robot", () => {
+  it(`should
+    - enter instructions,
+    - submit instructions,
+    - and return result for one robot`, () => {
     // given
     render(<App />);
 
@@ -38,8 +43,38 @@ describe("Martian Robots", () => {
     expect(listItems.at(0).textContent).toBe("Robot #1 result: 1 1 E");
   });
 
+  it(`should
+    - enter instructions for two robots,
+    - submit instructions,
+    - return results for both robots
+    - including a LOST message for robot two`, () => {
+    // given
+    render(<App />);
+
+    // when
+    const textAreaElement = screen.getByTestId("input-instruction");
+    fireEvent.change(textAreaElement, {
+      target: { value: "5 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL" },
+    });
+
+    const buttonElement = screen.getByRole("button");
+    fireEvent.click(buttonElement);
+
+    const main = screen.getByRole("main");
+    console.log(prettyDOM(main));
+
+    // then
+    const listItems = screen.getAllByRole("listitem");
+    expect(listItems.length).toBe(2);
+    expect(listItems.at(0).textContent).toBe("Robot #1 result: 1 1 E");
+    expect(listItems.at(1).textContent).toBe("Robot #2 result: 3 3 N LOST");
+  });
+
   // TODO - Need to figure out why the source code is off by one offset Y co-ordinate, as the PDF challenge output states a a diff ofset minus one...
-  it.skip("should enter instructions for two robots, submit, and return results for both robots", () => {
+  it.skip(`should
+    - enter instructions for two robots,
+    - submit instructions,
+    - return results for both robots without any LOST messages for both`, () => {
     // given
     render(<App />);
 
